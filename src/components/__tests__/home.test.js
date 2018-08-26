10bb67565a2f9e7b620416f600e35b865511b580
Kamar5 <qfk51994@gmail.com>
2018-08-26 10:31:58 -0400

    add test and modified eslint config

--- src/components/__tests__/home.test.js
+++ src/components/__tests__/home.test.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { PageHeader } from 'react-bootstrap';
+
 import Home from '../home';
+import GroupList from '../groupList';
+import SingleList from '../singleList';
+import FormControlComponent from '../formControl';
 
 describe('home', () => {
-
+    const wrapper = shallow(<Home />)
     it('renders without crashing', () => {
-        const wrapper = shallow(<Home />);
         expect(wrapper.length).toBe(1);
+    });
+
+    it('should render Page Header component', () => {
+        expect(wrapper.find(PageHeader).length).toBe(1);
+    });
+    it('should render FormControlComponent component', () => {
+        expect(wrapper.find(FormControlComponent).length).toBe(1);
+    });
+    it('should render GroupList component', () => {
+        expect(wrapper.find(GroupList).length).toBe(1);
+    });
+    it('should render SingleList component', () => {
+        expect(wrapper.find(SingleList).length).toBe(1);
     });
 
 });
